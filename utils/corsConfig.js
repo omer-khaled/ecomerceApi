da750f98555ff0e9797b469afd29556caed491d7
omer-khaled <omerkhaledmohamedmohamed123@gmail.com>
2023-10-16 16:56:13 +0300

    deployment deploy origin

--- utils/corsConfig.js
+++ utils/corsConfig.js
@@ -1,5 +1,5 @@
 const corsOption = {
-    origin:['https://omer-khaled.github.io','https://comerce-gibn5tydr-omer-khaleds-projects.vercel.app'],
+    origin:['https://omer-khaled.github.io','https://comerce-k1ndqcmy9-omer-khaleds-projects.vercel.app'],
     methods:'OPTIONS,GET,POST,PUT,PATCH,DELETE',
     allowedHeaders:'Content-Type,Authorization',
     credentials:true,
